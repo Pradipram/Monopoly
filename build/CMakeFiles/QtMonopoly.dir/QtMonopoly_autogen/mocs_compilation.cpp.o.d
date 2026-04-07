a813bdc388d6963888db24bc118ad081af2e3360
Pradip Ram <pradipramnawa@gmail.com>
2026-04-07 15:46:30 +0000

    Remove local build artifacts and update gitignore

--- build/CMakeFiles/QtMonopoly.dir/QtMonopoly_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/QtMonopoly.dir/QtMonopoly_autogen/mocs_compilation.cpp.o: \
- /home/pradip/Projects/QtMonopoly/build/QtMonopoly_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
